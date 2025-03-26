b5dcb898a01eebd771f3094689efc614d51a1152
sahilempire <gsahil.sg20@gmail.com>
2025-03-26 17:53:09 +0530

    port

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ app.post('/api/support', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-}); 
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${port}`);
+});
